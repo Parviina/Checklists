985d41e23af3fc2ddca2dbd022b563a2310ad5f2
Parvina <parvina1417@mail.ru>
2022-04-18 18:14:35 +0500

    We create checklist.

--- Checklists/MainViewController.swift
+++ Checklists/MainViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class MainViewController: UITableViewController {
+class MainViewController: UITableViewController  {
+    let groups: [ChecklistGroup] = [
+        ChecklistGroup(title: "Birthdays", imageName: "Birthdaypic"),
+        ChecklistGroup(title: "Groceries",imageName: "groceriespic"),
+        ChecklistGroup(title: "To Do", imageName: "totopic"),
+        ChecklistGroup(title: "Business Stuff", imageName: "pic")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let group: ChecklistGroup = groups[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = group.title
+        return cell
+    }
 }
 
---
+++ Checklists/Models/ChecklistGroup.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistGroup.swift
+//  Checklists
+//
+//  Created by New User on 18.04.2022.
+//
+
+import Foundation
+
+struct ChecklistGroup {
+  let title: String
+  let imageName: String
+    }
